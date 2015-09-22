7b8c8d1e02be425627a3edae9349bf34410635e4
w3cay <w3cay@foxmail.com>
2015-09-22 23:23:37 +0800

    add struct

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var less = require('gulp-less')
+var minifyCSS = require('gulp-minify-css')
+var rename = require('gulp-rename');
+var livereload = require('gulp-livereload');
+//CSS样式
+gulp.task('style', function () {
+      gulp.src('**.less')
+        .pipe(less())
+        .pipe(gulp.dest('src/'))
+        .pipe(minifyCSS())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('src/mini/'))
+        .pipe(livereload());
+});
+
+//监视文件
+gulp.task('watch', function () {
+	livereload.listen();
+    gulp.watch('**.less', ['style'])
+})
+// 默认任务
+gulp.task('default', ['style','watch']);
+
+
+
